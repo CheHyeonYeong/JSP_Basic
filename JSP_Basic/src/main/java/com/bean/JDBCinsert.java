2e7de28612cdb80cf0cfb0418beba2a51c79a00b
unknown <gusdudco6@naver.com>
2024-04-11 12:15:02 +0900

    🚀 Db
    jdbc 복숩

---
+++ JSP_Basic/src/main/java/com/bean/JDBCinsert.java
@@ -0,0 +1,60 @@
+package com.bean;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class JDBCinsert {
+
+	public static void main(String[] args) {
+		Scanner s  = new Scanner(System.in);
+		System.out.println("id>> ");
+		String id = s.next();
+		System.out.println("pw>> ");
+		String pw = s.next();
+		System.out.println("name>> ");
+		String name = s.next();
+		System.out.println("email>> ");
+		String email = s.next();
+
+		Connection conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into members values(?,?,?,?)";
+		
+		try {
+		     Class.forName("com.mysql.cj.jdbc.Driver");
+		     conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbctest", "root", "root");
+	         System.out.println(conn);
+	         System.out.println("데이터베이스 접속 성공.");
+	         pstmt = conn.prepareStatement(sql);
+	         
+	         pstmt.setString(1,id);
+	         pstmt.setString(2,pw);
+	         pstmt.setString(3,name);
+	         pstmt.setString(4,email);
+
+	            
+	         int result = pstmt.executeUpdate();
+	         if(result != 0) {
+	         	System.out.println("SQL성공");
+	         }else {
+	        	 System.out.println("SQL실패");
+	        	 }
+	         
+		}  catch (ClassNotFoundException e) {
+	         System.out.println("드라이버 로드 실패");
+	      }catch (SQLException sqle) {
+	         System.out.println("SQL 연동 오류");
+	         System.out.println(sqle.getMessage());
+	      }finally {
+	         try {
+	            
+	         } catch (Exception e) {
+	            // TODO: handle exception
+	         }
+	      }
+		}
+	}
+
